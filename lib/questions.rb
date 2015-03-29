7a56eb7147e056039ff207b65179a3762f39dee7
Kevin Lanzon <kevinlanzon@gmail.com>
2015-03-29 20:07:25 +0100

    corrected syntax errors

--- lib/questions.rb
+++ lib/questions.rb
@@ -67,7 +67,7 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
-  number = array.group_by {|num| num % 2}
+  number = array.group_by { |num| num % 2 }
   [number[0], number[1]]
 end
 
