fa45bced3f13d3678cf07766432ec04685ee90b8
DanielJohnston <macademy@danieljohnston.co.uk>
2015-06-15 14:36:33 +0100

    did a few more

--- lib/questions.rb
+++ lib/questions.rb
@@ -67,7 +67,7 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
-  [array.keep_if.with_index{|e, i|i%2==0},array.reject.with_index{|e, i|i%2==0}]
+  return [array.reject{|e|e%2==1},array.reject{|e|e%2==0}]
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -75,33 +75,40 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
+  array.inject(0){|accum, word| (word == word.reverse) ? accum+1 : accum}
 end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
+  array.inject{|accum, word| accum.length < word.length ? accum : word}
 end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
+  array.inject{|accum, word| accum.length > word.length ? accum : word}
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+  array.inject{|accum, e| accum+e}
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+  array+array
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  symbol.to_s
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  ((array.inject(:+).to_f)/array.length).round.to_i
 end
 
 # get all the elements in an array, up until the first element
@@ -109,12 +116,14 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
+  array.first(array.find_index{|x|x>5})
 end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+  [array.reject.with_index{|e, i|i%2==1},array.reject.with_index{|e, i|i%2==0}].transpose.to_h
 end
 
 # get all the letters used in an array of words and return
@@ -122,12 +131,14 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
+  array.join.split(//).uniq.sort
 end
 
 # swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
+  
 end
 
 # in a hash where the keys and values are all numbers
