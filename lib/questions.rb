eef3c92307f17ac3808e1153265a3bba6c750f2d
Gabrielle <gabivk@gmail.com>
2016-04-23 14:00:13 +0100

    passing test 17

--- lib/questions.rb
+++ lib/questions.rb
@@ -91,11 +91,13 @@ end
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+  array.reduce(&:+)
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+  array * 2
 end
 
 # convert a symbol into a string
