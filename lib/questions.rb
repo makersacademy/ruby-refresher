479322f6b94f24c96463b3453b3db89cf79b67e3
Gabrielle <gabivk@gmail.com>
2016-04-23 13:44:01 +0100

    passing test 11

--- lib/questions.rb
+++ lib/questions.rb
@@ -21,7 +21,7 @@ end
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(array)
-  array.map { |word| word.reverse }
+  array.map(&:reverse)
 end
 
 # given an array of student names, like ['Bob', 'Dave', 'Clive']
@@ -46,19 +46,20 @@ end
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
-  array.sort_by! { |word| word[-1]}
+  array.sort_by! { |word| word[-1] }
 end
 
 # cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
-  string[0..2]
+  string[0...(string.length/2.0).round]
 end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+  -(number.abs)
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of
