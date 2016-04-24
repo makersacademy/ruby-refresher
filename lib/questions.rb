d0d97925b5b986b2be987c0f9d19cdd231b4fc6c
Murilo Dal Ri <murilodalri@gmail.com>
2016-04-24 18:41:05 +0100

    changed one method

--- lib/questions.rb
+++ lib/questions.rb
@@ -16,7 +16,7 @@ end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
-  array.compact!.select! { |element| element }
+  array.select! { |element| element }
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
