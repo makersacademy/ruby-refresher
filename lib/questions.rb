f52aed5298fad5979ed65f10e3b575318bb6e328
hannako <jessjones1@gmail.com>
2016-08-19 19:54:51 +0100

    11-20

--- lib/questions.rb
+++ lib/questions.rb
@@ -61,6 +61,7 @@ end
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+  number.abs*-1
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of
@@ -68,6 +69,7 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+  array.partition { |number| number.even? }
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -75,33 +77,41 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
+  array.keep_if{|word| word == word.reverse}.length
 end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
+  array.sort_by{|word|word.length}[0]
 end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
+  array.sort_by{|word|word.length}[-1]
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+  array.reduce(:+)
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+  return array+=array
+
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  symbol.to_s
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  return (array.reduce(:+)/array.length.to_f).round
 end
 
 # get all the elements in an array, up until the first element
@@ -109,6 +119,7 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
+  array.take_while{|i| i<6}
 end
 
 # turn an array (with an even number of elements) into a hash, by
