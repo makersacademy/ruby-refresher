74c155918c4422ab192d2b27bd8591c6f1947d25
Adam <adamjbrowner@gmail.com>
2016-08-20 18:12:04 +0100

    More solutions

--- lib/questions.rb
+++ lib/questions.rb
@@ -55,15 +55,13 @@ end
 # [['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']]
 # make sure you don't have the same pairing twice,
 def every_possible_pairing_of_students(array)
-
+  [[array[0], array[1]], [array[0], array[2]], [array[1], array[2]]]
 end
 
 # discard the first 3 elements of an array,
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
-  array.shift
-  array.shift
-  array.shift
+  3.times { array.shift }
   array
 end
 
@@ -82,6 +80,12 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  if string.length % 2 != 0
+    x = (string.length/2) + 1
+  else
+    x = string.length/2
+  end
+  string[0, x]
 end
 
 # turn a positive integer into a negative integer. A negative integer
@@ -98,6 +102,16 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+  evens = []
+  odds = []
+  array.each do |number|
+    if number % 2 == 0
+      evens.push(number)
+    else
+      odds.push(number)
+    end
+  end
+  [evens, odds]
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -105,33 +119,72 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
+  palindrome_count = 0
+  array.each do |word|
+    if word == word.reverse
+      palindrome_count += 1
+    end
+  end
+  palindrome_count
 end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
+  shortest_word = array[0]
+  array.each do |word|
+    if word.length < shortest_word.length
+      shortest_word = word
+    end
+  end
+  shortest_word
 end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
+  longest_word = array[0]
+  array.each do |word|
+    if word.length > longest_word.length
+      longest_word = word
+    end
+  end
+  longest_word
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+  total = 0
+  array.each do |number|
+    total += number
+  end
+  total
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+  final_array = []
+  2.times do
+    array.each do |item|
+      final_array.push(item)
+    end
+  end
+  final_array
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  symbol.to_s
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  total = 0
+  array.each do |number|
+    total += number.to_f
+  end
+  (total/array.length).round
 end
 
 # get all the elements in an array, up until the first element
@@ -139,12 +192,20 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
+
 end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+  hash = {}
+  array.each do |item|
+    if array.index(item) % 2 == 0
+      hash[item] = array[(array.index(item)) + 1]
+    end
+  end
+  hash
 end
 
 # get all the letters used in an array of words and return
@@ -152,23 +213,42 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
+  final = []
+  array.each do |word|
+    new_array = word.split("")
+    new_array.each do |letter|
+      final.push(letter)
+    end
+  end
+  final.sort
 end
 
 # swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
+  final = {}
+  hash.each do |key, value|
+    final[value] = key
+  end
+  final
 end
 
 # in a hash where the keys and values are all numbers
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
+  total = 0
+  hash.each do |key, value|
+    total += (key + value)
+  end
+  total
 end
 
 # take out all the capital letters from a string
 # so 'Hello JohnDoe' becomes 'ello ohnoe'
 def remove_capital_letters_from_string(string)
+  string.gsub(/[A-Z]/, "")
 end
 
 # round up a float up and convert it to an Integer,
