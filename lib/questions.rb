4f66ff4ad61f14a43dfb0e2bc33b00a065988f20
cyberplanner <saleh.gfx@gmail.com>
2016-08-21 14:41:39 +0100

    Solves#14, solves#15

--- lib/questions.rb
+++ lib/questions.rb
@@ -87,10 +87,12 @@ end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
+  array.sort { |one, two| one.length <=> two.length }.first
 end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
+  array.sort { |one, two| one.length <=> two.length }.last
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
