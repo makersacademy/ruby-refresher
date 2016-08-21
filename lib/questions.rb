31e659df97422bdd8cf7648bcfa09991380b427a
Jose <jcopoviking@outlook.com>
2016-08-21 23:54:58 +0100

    adds q 21

--- lib/questions.rb
+++ lib/questions.rb
@@ -136,11 +136,12 @@ def get_elements_until_greater_than_five(array)
   array.take_while {|n| n <= 5}
 end
 #
-# # turn an array (with an even number of elements) into a hash, by
-# # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
-# # {'a' => 'b', 'c' => 'd'}
-# def convert_array_to_a_hash(array)
-# end
+# turn an array (with an even number of elements) into a hash, by
+# pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
+# {'a' => 'b', 'c' => 'd'}
+def convert_array_to_a_hash(array)
+  Hash[*array]
+end
 #
 # # get all the letters used in an array of words and return
 # # it as a array of letters, in alphabetical order
