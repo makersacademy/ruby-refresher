e4029bb736ef7237883d35e2f29309326a7c1955
Nomi Vos <nvoswork@gmail.com>
2016-08-21 05:36:49 -0500

    passes test 28

--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -150,11 +150,11 @@ describe 'the Friday test :)' do
     n = round_down_number 4.9
     expect(n).to eq 4
   end
-  #
-  # it 'format_date_nicely' do
-  #   n = format_date_nicely Time.new(2013, 10, 31)
-  #   expect(n).to eq '31/10/2013'
-  # end
+
+  it 'format_date_nicely' do
+    n = format_date_nicely Time.new(2013, 10, 31)
+    expect(n).to eq '31/10/2013'
+  end
   #
   # it 'get_domain_name_from_email_address' do
   #   n = get_domain_name_from_email_address 'alex@makersacademy.com'
