b3eac285ddb489f715758c3a5ed12272b5587619
Adam <adamjbrowner@gmail.com>
2016-08-21 19:41:56 +0100

    Bottles of beer done

--- lib/questions.rb
+++ lib/questions.rb
@@ -374,4 +374,13 @@ end
 # at the end.
 # (there's no RSpec test for this one)
 def ninety_nine_bottles_of_beer
+  bottles = 99
+  while bottles > 1
+    puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
+    Take one down and pass it around, #{bottles - 1} bottles of beer on the wall."
+    bottles -= 1
+  end
+  puts "1 bottle of beer on the wall, 1 bottle of beer.
+    Take one down and pass it around, no more bottles of beer on the wall."
+  puts "No more bottles of beer on the wall"
 end
