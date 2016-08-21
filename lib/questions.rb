4348ddebe580246703409986457d2140d0955ce0
liskowsky <karollebiedzinski@gmail.com>
2016-08-21 13:20:43 +0100

    34/39

--- lib/questions.rb
+++ lib/questions.rb
@@ -178,7 +178,7 @@ end
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
 # becomes 31/10/2013
 def format_date_nicely(date)
-  date.strftime('%d/%m/%Y')
+  date.strftime("%d/%m/%Y")
 end
 
 # get the domain name *without* the .com part, from an email address
@@ -193,26 +193,40 @@ end
 # 'the lion the witch and the wardrobe' becomes
 # 'The Lion the Witch and the Wardrobe'
 def titleize_a_string(string)
+  skip = ["a","and","the"]
+  new_string = []
+  string.split(" ").each_with_index do |x,i|
+    if !(skip.include? x) || (i == 0)
+      new_string.push x.capitalize
+    else
+      new_string.push x
+    end
+  end
+  new_string.join(" ")
 end
 
 # return true if a string contains any special characters
 # where 'special character' means anything apart from the letters
 # a-z (uppercase and lower) or numbers
 def check_a_string_for_special_characters(string)
+  string != string[/\A[a-zA-Z0-9]*\z/]
 end
 
 # get the upper limit of a range. e.g. for the range 1..20, you
 # should return 20
 def get_upper_limit_of(range)
+  range.end
 end
 
 # should return true for a 3 dot range like 1...20, false for a
 # normal 2 dot range
 def is_a_3_dot_range?(range)
+  range.exclude_end?
 end
 
 # get the square root of a number
 def square_root_of(number)
+  Math.sqrt(number)
 end
 
 # count the number of words in a file
