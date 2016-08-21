e8e0ae8eab1644d57e4c5e3fbeb2ddb5b297a722
cyberplanner <saleh.gfx@gmail.com>
2016-08-21 19:22:06 +0100

    Solves#18, solves#19

--- lib/questions.rb
+++ lib/questions.rb
@@ -109,11 +109,13 @@ end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  symbol.to_s
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  (array.inject(:+) / array.count.to_f).round
 end
 
 # get all the elements in an array, up until the first element
