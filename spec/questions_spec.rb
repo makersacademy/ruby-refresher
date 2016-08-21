2d074802f6730ba3b8caeadb62385da2c8da7329
Nomi Vos <nvoswork@gmail.com>
2016-08-21 14:13:55 -0500

    passes test 36

--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -198,10 +198,10 @@ describe 'the Friday test :)' do
     n = word_count_a_file 'data/lorem.txt'
     expect(n).to eq 70
   end
-  #
-  # it 'call_method_from_string' do
-  #   expect { call_method_from_string('foobar') }.to raise_error(NameError)
-  # end
+
+  it 'call_method_from_string' do
+    expect { call_method_from_string('foobar') }.to raise_error(NameError)
+  end
   #
   # it 'is_a_2014_bank_holiday?' do
   #   a = is_a_2014_bank_holiday?(Time.new(2014, 8, 25))
