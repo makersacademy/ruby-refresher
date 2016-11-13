688ac55b71192a156810e3254c6d7e0ddde64854
alexanderwjrussell <awjrussell@live.co.uk>
2016-11-13 11:43:11 +0000

    Shortest and longest words in array tests passing.

--- lib/questions.rb
+++ lib/questions.rb
@@ -80,10 +80,12 @@ end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
+  array.min{|a,b| a.size <=> b.size }
 end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
+  array.max{|a,b| a.size <=> b.size }
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
