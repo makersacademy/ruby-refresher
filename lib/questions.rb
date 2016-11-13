6d3dbb64b666266b91cbd3d148fb85f6b2967661
amidabrian51 <briankaty1@me.com>
2016-11-13 19:24:14 +0000

    the second commit

--- lib/questions.rb
+++ lib/questions.rb
@@ -53,12 +53,15 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  half = (string.size/2.to_f).ceil
+  string[1:half]
 
 end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of
