bb1da4afffce5c22326ff3247c613462d4249712
alexanderwjrussell <awjrussell@live.co.uk>
2016-11-13 11:55:12 +0000

    Average of array test passing.

--- lib/questions.rb
+++ lib/questions.rb
@@ -108,6 +108,8 @@ end
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  total = array.inject(0, :+)
+  average = (total.to_f / array.length).round
 end
 
 # get all the elements in an array, up until the first element
