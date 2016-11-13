84f76c1baaa82154f762c683e1436c8fadc48d72
alexanderwjrussell <awjrussell@live.co.uk>
2016-11-13 11:29:30 +0000

    Adding to the beginning of an array test now passing.

--- lib/questions.rb
+++ lib/questions.rb
@@ -35,10 +35,12 @@ end
 # discard the first 3 elements of an array,
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
+  array[3..array.length]
 end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+  array.unshift(element)
 end
 
 # sort an array of words by their last letter, e.g.
