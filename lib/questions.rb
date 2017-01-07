32ed1b180c1c00bd1322f7c90e2feb61f2c41563
keomony khun <keomony@gmail.com>
2017-01-07 14:45:02 +0000

    refactors using min in shortest word in an array

--- lib/questions.rb
+++ lib/questions.rb
@@ -80,11 +80,12 @@ end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
-  array.sort{|a| a.length}.first
+  array.sort{|a| a.length}.min
 end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
+
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
