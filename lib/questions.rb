d320b2857a5e687135996fbec663a57a5e3f6e81
Chris Cooper <cjcooper252@gmail.com>
2017-01-07 14:53:44 +0000

    9 to go

--- lib/questions.rb
+++ lib/questions.rb
@@ -175,21 +175,25 @@ end
 # round up a float up and convert it to an Integer,
 # so 3.214 becomes 4
 def round_up_number(float)
+  float.ceil
 end
 
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+  float.floor
 end
 
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
 # becomes 31/10/2013
 def format_date_nicely(date)
+  date.strftime("%d/%m/%Y")
 end
 
 # get the domain name *without* the .com part, from an email address
 # so alex@makersacademy.com becomes makersacademy
 def get_domain_name_from_email_address(email)
+  email.split("@")[1].split(".")[0]
 end
 
 # capitalize the first letter in each word of a string,
@@ -198,6 +202,14 @@ end
 # 'the lion the witch and the wardrobe' becomes
 # 'The Lion the Witch and the Wardrobe'
 def titleize_a_string(string)
+  result = string.split.map do |word|
+              if ["a","the","and"].include?(word)
+                word
+              else
+                word.capitalize
+              end
+            end.join(' ')
+  result.slice(0,1).capitalize + result.slice(1..-1)
 end
 
 # return true if a string contains any special characters
