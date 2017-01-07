7a27de35ff4f8df2e6ab9a0e5691e7406397f437
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-07 14:58:56 +0000

    get first half of string rounded up

--- lib/questions.rb
+++ lib/questions.rb
@@ -55,6 +55,8 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  string.length.odd? ? half = (string.length/2) + 1 : half = string.length/2
+  string.slice(0, half)
 end
 
 # turn a positive integer into a negative integer. A negative integer
