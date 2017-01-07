37c1d137ce5b96aa752c9f460831905c61d9b5ed
keomony khun <keomony@gmail.com>
2017-01-07 21:57:30 +0000

    refactors by using split(%r{\s*}) get all letters in array of word return them in order

--- lib/questions.rb
+++ lib/questions.rb
@@ -130,7 +130,7 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
-  array.map{|a| a.chars}.flatten.sort
+  array.join(" ").split(%r{\s*}).sort
 end
 
 # swap the keys and values in a hash. e.g.
