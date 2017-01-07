5d20ce7259c640d8511c7f6eaf159c8622e49ff7
Dan Pelensky <dan@pelensky.com>
2017-01-07 11:41:57 +0000

    12 completed

--- lib/questions.rb
+++ lib/questions.rb
@@ -46,17 +46,29 @@ end
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
+  array.sort_by{|word|word[-1]}
 end
 
 # cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  if string.length % 2 != 0
+    characters = (string.length + 1)/2
+  else
+    characters = (string.length)/2
+  end
+  string[0..characters - 1]
 end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+  if number < 0
+    number
+  else
+    number * -1
+  end
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of
@@ -64,6 +76,9 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+  even = array.select{|num| num % 2 == 0}
+  odd = array.select{|num| num % 2 != 0}
+  [even, odd]
 end
 
 # count the numbers of elements in an element which are palindromes
