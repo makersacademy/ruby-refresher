0e68ef46404fe49582e72ec5ef506f5dc2fe5cf7
ajbater <amanda22@hotmail.co.uk>
2017-01-07 13:12:27 +0000

    11th test passing, refactored 10th solution into 1 liner

--- lib/questions.rb
+++ lib/questions.rb
@@ -54,16 +54,13 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
-  if string.length.even?
-    string[0..(string.length / 2) - 1]
-  else
-    string[0..string.length / 2]
-  end
+  string.length.even? ? string[0..(string.length / 2) - 1] : string[0..string.length / 2]
 end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+  number > 0 ? number * -1 : number
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of
