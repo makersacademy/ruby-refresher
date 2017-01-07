252b4a1a69fc27f15b8faf09ccf4e4775a6cef6e
keomony khun <keomony@gmail.com>
2017-01-07 11:56:55 +0000

    removes nil or false from an array

--- lib/questions.rb
+++ lib/questions.rb
@@ -16,6 +16,7 @@ end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
+  array.select{|a| nil|a}
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
