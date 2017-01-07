3803babcebbfec3812d7fccdfdf07fa65c99eba1
Kim Wilson <k.wilson541@gmail.com>
2017-01-07 18:08:55 +0000

    Completed method check_a_string_for_special_characters, test passing

--- lib/questions.rb
+++ lib/questions.rb
@@ -194,6 +194,7 @@ end
 # where 'special character' means anything apart from the letters
 # a-z (uppercase and lower) or numbers
 def check_a_string_for_special_characters(string)
+	!string.gsub(/[A-Za-z0-9]*/, "").empty?
 end
 
 # get the upper limit of a range. e.g. for the range 1..20, you
