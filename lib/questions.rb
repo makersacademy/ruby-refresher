f1e36f80384b40718c50679ba35634093a7cb411
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-07 11:01:05 +0000

    passed test 2

--- lib/questions.rb
+++ lib/questions.rb
@@ -5,6 +5,7 @@ end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+  array.select{|item| item =~ /^[aeiou]/}pas
 end
 
 # remove instances of nil (but NOT false) from an array
