757eaad7a38c6caedde2718cf0c7ff4523b2dc17
George Eaton <george_eaton@hotmail.co.uk>
2017-01-07 18:58:27 +0000

    5 complete

--- lib/questions.rb
+++ lib/questions.rb
@@ -5,23 +5,23 @@ end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
-  array.select{ |element| element.match(/\A[aeiou]/) }
+  array.select { |element| element.match(/\A[aeiou]/) }
 end
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
-  array.reject{ |element| element == nil }
+  array.reject { |element| element == nil }
 end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
-  array.reject{ |element| element == nil || element == false }
-
+  array.reject { |element| element == nil || element == false }
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(array)
+  array.each { |element| element.reverse! }
 end
 
 # given an array of student names, like ['Bob', 'Dave', 'Clive']
