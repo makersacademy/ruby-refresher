885d282f2d0beabc95c356ae7e45e4cd657a276e
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-07 11:53:19 +0000

    passed test 8

--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -45,7 +45,7 @@ describe 'the Friday test :)' do
     expect(n).to eq [4, 5, 6, 7]
   end
 
-  xit 'add_element_to_beginning_of_array' do
+  it 'add_element_to_beginning_of_array' do
     n = add_element_to_beginning_of_array [2, 3, 4, 5], 1
     expect(n).to eq [1, 2, 3, 4, 5]
   end
