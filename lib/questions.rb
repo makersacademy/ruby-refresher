4c1395a160ff35dc51ed772f5368265cd2c9b25e
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-07 18:31:13 +0000

    Exercise 10 done

--- lib/questions.rb
+++ lib/questions.rb
@@ -55,6 +55,8 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  new_length = (string.length/2.to_f).ceil
+  string = string[0..(new_length - 1)]
 end
 
 # turn a positive integer into a negative integer. A negative integer
