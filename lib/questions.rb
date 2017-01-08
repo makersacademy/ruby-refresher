6b685f58374c39b1712a33240b1f02e92729379e
TudorTacal <tudortacal@gmail.com>
2017-01-08 11:45:12 +0000

    8, 9 done

--- lib/questions.rb
+++ lib/questions.rb
@@ -36,10 +36,13 @@ end
 # discard the first 3 elements of an array,
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
+  array.select!{|element| array[element-1] > 3}
+  # just because the array is made of numbers :)
 end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+  array.unshift(element)
 end
 
 # sort an array of words by their last letter, e.g.
