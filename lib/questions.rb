dc45ed4b34204da6d8e19ccceceb875df0b28934
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-08 14:07:28 +0000

    Exercise 24 done

--- lib/questions.rb
+++ lib/questions.rb
@@ -169,6 +169,8 @@ end
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
+  array = hash.to_a.flatten!
+  result = array.inject { |a, b| a + b }
 end
 
 # take out all the capital letters from a string
