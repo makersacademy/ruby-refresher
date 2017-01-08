f522aefc2c1000a3f1833761dc1d15b623f60222
George Eaton <george_eaton@hotmail.co.uk>
2017-01-08 09:51:44 +0000

    20 complete

--- lib/questions.rb
+++ lib/questions.rb
@@ -131,6 +131,11 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
+  less_than_6 = []
+  array.each do | element |
+    return less_than_6 if element > 5
+    less_than_6 << element if element < 6
+  end
 end
 
 # turn an array (with an even number of elements) into a hash, by
