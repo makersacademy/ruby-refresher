2557bfbdea1073f11aec3c0287feb13d8089cf90
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-08 10:15:49 +0000

    passed test 15

--- lib/questions.rb
+++ lib/questions.rb
@@ -95,6 +95,7 @@ end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
+  array.sort_by{|elem| elem.length}.last
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
