d5f44c146931f1b65ce8319e98217e8e1f2fd375
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-08 13:39:42 +0000

    Exercise 20 done

--- lib/questions.rb
+++ lib/questions.rb
@@ -129,6 +129,13 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
+  result = []
+  n = 0
+  until array[n] > 5
+    result << array[n]
+    n += 1
+  end
+  result
 end
 
 # turn an array (with an even number of elements) into a hash, by
