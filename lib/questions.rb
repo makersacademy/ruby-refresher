6dc80cf69c4561d4d22cb628b0161c1d73f85527
Asuka Ochi <asukaochi@gmail.com>
2017-01-08 20:21:02 +0000

    done get_all_letters_in_array_of_words

--- lib/questions.rb
+++ lib/questions.rb
@@ -138,6 +138,8 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
+  letters = array.join("")
+  letters.split("").sort
 end
 
 # swap the keys and values in a hash. e.g.
