689879efb135013191a4a162e85b8a97c74d20b7
keomony khun <keomony@gmail.com>
2017-01-08 14:56:06 +0000

    ninety-nine bottles of beer

--- lib/questions.rb
+++ lib/questions.rb
@@ -260,5 +260,74 @@ end
 # beer on the wall, and print 'no more bottles of beer on the wall'
 # at the end.
 # (there's no RSpec test for this one)
-def ninety_nine_bottles_of_beer
+def english_number number
+
+    num_string = ''  # This is the string we will return.
+    ones_place = ['one',     'two',       'three',    'four',     'five',
+                 'six',     'seven',     'eight',    'nine']
+    tens_place = ['ten',     'twenty',    'thirty',   'forty',    'fifty',
+                 'sixty',   'seventy',   'eighty',   'ninety']
+    teenagers = ['eleven',  'twelve',    'thirteen', 'fourteen', 'fifteen',
+                 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+    left  = number
+    write = left/100          # How many hundreds left to write out?
+    left  = left - write*100  # Subtract off those hundreds.
+
+    if write > 0
+      hundreds  = english_number write
+      num_string = num_string + hundreds + ' hundred'
+
+      if left > 0
+        num_string = num_string + ' and '
+      end
+    end
+
+    write = left/10          # How many tens left to write out?
+    left  = left - write*10  # Subtract off those tens.
+
+    if write > 0
+      if ((write == 1) and (left > 0))
+        num_string = num_string + teenagers[left-1]
+        left = 0
+      else
+        num_string = num_string + tens_place[write-1]
+      end
+
+      if left > 0
+        num_string = num_string + '-'
+      end
+    end
+
+    write = left  # How many ones left to write out?
+    left  = 0     # Subtract off those ones.
+
+    if write > 0
+      num_string = num_string + ones_place[write-1]
+    end
+    num_string
+
 end
+
+def ninety_nine_bottles_of_beer
+  i, init_value = 99, 99
+  print_bottles = 'bottles'
+
+  while i > 0
+
+    print_bottles = 'bottle' if i == 1
+  	puts "#{english_number(i)} #{print_bottles} of beer on the wall, #{english_number(i)} #{print_bottles} of beer."
+  	i -=1
+    print_bottles = 'bottle' if i == 1
+    if i != 0
+      puts "Take one down and pass it around, #{english_number(i)} #{print_bottles} of beer on the wall.\n\n"
+    else
+     puts "Take one down and pass it around, no more bottles of beer on the wall."
+     puts "No more bottles of beer on the wall, no more bottles of beer."
+     puts "Go to the store and buy some more, #{english_number(init_value)} bottles of beer on the wall."
+   end
+
+  end
+
+end
+ninety_nine_bottles_of_beer
