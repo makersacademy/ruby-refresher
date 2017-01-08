cd4683d7364991f9366d9d4293d29fce9df64056
ejatkin <ejatkin@gmail.com>
2017-01-08 15:40:44 +0000

    Nineteenth test passing

--- lib/questions.rb
+++ lib/questions.rb
@@ -117,6 +117,8 @@ end
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  answer = (array.inject(:+).to_f) / (array.length.to_f)
+  answer.ceil
 end
 
 # get all the elements in an array, up until the first element
