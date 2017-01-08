dc2437747a0695d2cddf44181e7ca9f8f6d1d2e3
ejatkin <ejatkin@gmail.com>
2017-01-08 18:34:50 +0000

    Thirty two tests passing

--- lib/questions.rb
+++ lib/questions.rb
@@ -225,6 +225,10 @@ end
 # should return true for a 3 dot range like 1...20, false for a
 # normal 2 dot range
 def is_a_3_dot_range?(range)
+  if range.exclude_end?
+    return true
+  else return false
+  end
 end
 
 # get the square root of a number
