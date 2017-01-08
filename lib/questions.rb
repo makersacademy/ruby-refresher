900933767ef1aea6e264c825d75dde7e41ee595a
George Eaton <george_eaton@hotmail.co.uk>
2017-01-08 11:43:33 +0000

    40 complete

--- lib/questions.rb
+++ lib/questions.rb
@@ -322,4 +322,20 @@ end
 # at the end.
 # (there's no RSpec test for this one)
 def ninety_nine_bottles_of_beer
+  count = 99
+
+while count > 1
+    puts "#{count} bottles of beer on the wall, #{count} bottles of beer."
+    count -= 1
+    if count > 1
+      puts "Take one down and pass it around, #{count} bottles of beer on the wall."
+    elsif count == 1
+      puts "Take one down and pass it around, #{count} bottle of beer on the wall."
+      puts "#{count} bottle of beer on the wall, #{count} bottle of beer."
+      puts "Take one down and pass it around, no more bottles of beer on the wall."
+    end
+  end
+  puts "No more bottles of beer on the wall, no more bottles of beer."
+  puts "Go to the store and buy some more, 99 bottles of beer on the wall."
 end
+p ninety_nine_bottles_of_beer
