4f854848cb47ec8a7aaacc4516b21f9f36b9dc78
TudorTacal <tudortacal@gmail.com>
2017-01-08 12:44:27 +0000

    15 done

--- lib/questions.rb
+++ lib/questions.rb
@@ -55,13 +55,15 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
-  return string[0..string.length/2] if string.length % 2 != 0 
+  return string[0..string.length/2] if string.length % 2 != 0
   string[0,string.length/2]
 end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+  return number if number < 0
+  number -= number * 2
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of
@@ -69,6 +71,16 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+  result = []
+  even = []
+  odd  = []
+  array.each do |element|
+    odd.push(element) if element % 2 != 0
+    even.push(element) if element % 2 == 0
+  end
+  result << even
+  result << odd
+  result
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -76,10 +88,14 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
+  count = 0
+  array.each{|element| count +=1 if element == element.reverse }
+  count
 end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
+  array.sort_by{|element| element.length}.shift
 end
 
 # return the shortest word in an array
