28d8bf5166994d677d1c5ff437d82c49dbd5a60b
Ross Benzie <rossbenzie@gmail.com>
2017-02-18 12:17:19 +0000

    average_of_array

--- lib/questions.rb
+++ lib/questions.rb
@@ -91,33 +91,52 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
+  num_pals = 0
+  array.each do |word|
+    num_pals +=1 if word.reverse == word
+  end
+  num_pals
 end
 
 # return the shortest word in an array
-def shortest_word_in_array(array)
+def shortest_word_in_array(arr)
+  shortest = arr.first
+  arr.each do |word|
+    shortest = word if word.size < shortest.size
+  end
+  shortest
 end
 
 # return the shortest word in an array
-def longest_word_in_array(array)
+def longest_word_in_array(arr)
+  longest = arr.first
+  arr.each do |word|
+    longest = word if word.size > longest.size
+  end
+  longest
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+  array.reduce(0, :+)
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+  array += array
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  symbol.to_s
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  total_of_array(array).fdiv(array.size).round
 end
 
 # get all the elements in an array, up until the first element
