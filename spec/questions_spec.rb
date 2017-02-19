d819168fe59c739937e8256daaf8dcd8cabc2167
Ekaterina Loshchinina <KateLoschinina@ekaterinasmbp5.home>
2017-02-19 22:35:24 +0000

    Completed exercise 32.

--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -174,7 +174,7 @@ describe 'the Friday test :)' do
     expect(b).to be true
   end
 
-  it '32. get_upper_limit_of' do
+  fit '32. get_upper_limit_of' do
     n = get_upper_limit_of 1..20
     expect(n).to eq 20
   end
