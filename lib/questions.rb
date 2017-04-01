b55bf5825c0dd7c909cfb9bb7a18497fcde9d7d1
Ashwini <ashie.oh@gmail.com>
2017-04-01 07:51:51 +0100

    Finishes 33 questions

--- lib/questions.rb
+++ lib/questions.rb
@@ -185,22 +185,29 @@ end
 # 'the lion the witch and the wardrobe' becomes
 # 'The Lion the Witch and the Wardrobe'
 def titleize_a_string(string)
+  exclude = ['and', 'a', 'the']
+  string.split(" ").map.with_index do |word, index|
+   (exclude.include?(word) && index != 0) ? word : word.capitalize
+  end.join(' ')
 end
 
 # return true if a string contains any special characters
 # where 'special character' means anything apart from the letters
 # a-z (uppercase and lower) or numbers
 def check_a_string_for_special_characters(string)
+  !!(string.match(/[^a-z0-9]+/i))
 end
 
 # get the upper limit of a range. e.g. for the range 1..20, you
 # should return 20
 def get_upper_limit_of(range)
+  range.max
 end
 
 # should return true for a 3 dot range like 1...20, false for a
 # normal 2 dot range
 def is_a_3_dot_range?(range)
+  range.exclude_end?
 end
 
 # get the square root of a number
