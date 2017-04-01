abb6fb4d69f57ed2271c8926498791a8589bc3ae
Katie Koschland <katiekoschland@hotmail.com>
2017-04-01 15:57:48 +0100

    average of array

--- lib/questions.rb
+++ lib/questions.rb
@@ -97,15 +97,18 @@ end
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+  array * 2
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  symbol.to_s
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  array.inject(:+) / array.length + 1
 end
 
 # get all the elements in an array, up until the first element
@@ -113,6 +116,7 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
+
 end
 
 # turn an array (with an even number of elements) into a hash, by
