ef33fd76be49c4a38557fb0024066bd382b70a7e
Katie Koschland <katiekoschland@hotmail.com>
2017-04-01 11:21:07 +0100

    every possible pairing of students

--- lib/questions.rb
+++ lib/questions.rb
@@ -29,9 +29,7 @@ end
 # [['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']]
 # make sure you don't have the same pairing twice,
 def every_possible_pairing_of_students(array)
-  array = 'Bob', 'Dave', 'Clive'
-  array = array.repeated_combination(2)
-
+  array.combination(2).to_a
 end
 
 # discard the first 3 elements of an array,
