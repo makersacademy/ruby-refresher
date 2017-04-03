8e60091369598fb51dcff5cad2860ad79ee33aa0
Katie Koschland <katiekoschland@hotmail.com>
2017-04-03 07:42:20 +0100

    word count of a file

--- lib/questions.rb
+++ lib/questions.rb
@@ -69,6 +69,7 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+  [array.select{|x| x.even?}, array.select{|x| x.odd?}]
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -76,6 +77,7 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
+  array.count { |a| a.reverse == a }
 end
 
 # return the shortest word in an array
@@ -116,6 +118,9 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
+    new_array = []
+   array.each { |x| break if (x > 5); new_array.push(x) }
+   new_array
 
 end
 
@@ -123,6 +128,7 @@ end
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+  Hash[*array]
 end
 
 # get all the letters used in an array of words and return
@@ -136,6 +142,7 @@ end
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
+   hash.invert
 end
 
 # in a hash where the keys and values are all numbers
@@ -170,6 +177,7 @@ end
 # get the domain name *without* the .com part, from an email address
 # so alex@makersacademy.com becomes makersacademy
 def get_domain_name_from_email_address(email)
+  email.sub!(".com","").sub!(/^[^@]*@/,"")
 end
 
 # capitalize the first letter in each word of a string,
@@ -189,12 +197,13 @@ end
 # get the upper limit of a range. e.g. for the range 1..20, you
 # should return 20
 def get_upper_limit_of(range)
-
+  range.max
 end
 
 # should return true for a 3 dot range like 1...20, false for a
 # normal 2 dot range
 def is_a_3_dot_range?(range)
+  range.inspect.count(".") === 3
 end
 
 # get the square root of a number
@@ -204,7 +213,7 @@ end
 
 # count the number of words in a file
 def word_count_a_file(file_path)
-
+  File.open("data/lorem.txt").gets.split(" ").length
 end
 
 # --- tougher ones ---
