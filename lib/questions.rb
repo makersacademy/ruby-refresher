8ef1643e5ac30aa8ed9e627da2b368f0c18616a4
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-04-29 13:41:53 +0100

    Another 5 exercises completed

--- lib/questions.rb
+++ lib/questions.rb
@@ -65,6 +65,7 @@ end
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+  number > 0 ? -(number) : number
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of
@@ -72,6 +73,10 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+  even = array.select { |num| num.even? }
+  odd = array.select { |num| num.odd? }
+  array = []
+  array = array.push(even, odd)
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -79,14 +84,18 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
+  palindrome_occurances = array.select { |word| word == word.reverse }
+  palindrome_occurances.count
 end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
+  array.min{|a,b| a.size <=> b.size }
 end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
+  array.max{|a,b| a.size <=> b.size }
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
