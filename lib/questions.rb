229e2052fb0e95f43c4db90312bfd62e94080a1e
emmpak <eiliev05@yahoo.com>
2017-04-29 22:47:25 +0100

    Next 5

--- lib/questions.rb
+++ lib/questions.rb
@@ -15,7 +15,7 @@ end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
-  array.select{ |element| !!element }
+  array.select{ |element| element }
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
@@ -106,6 +106,7 @@ end
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  array.reduce(&:+)/(array.length).round
 end
 
 # get all the elements in an array, up until the first element
@@ -143,16 +144,19 @@ end
 # take out all the capital letters from a string
 # so 'Hello JohnDoe' becomes 'ello ohnoe'
 def remove_capital_letters_from_string(string)
+  string.gsub(/[A-Z]/,"")
 end
 
 # round up a float up and convert it to an Integer,
 # so 3.214 becomes 4
 def round_up_number(float)
+  float.ceil.to_i
 end
 
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+  float.floor.to_i
 end
 
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
@@ -163,6 +167,7 @@ end
 # get the domain name *without* the .com part, from an email address
 # so alex@makersacademy.com becomes makersacademy
 def get_domain_name_from_email_address(email)
+  email.split(/[@.]/)[1]
 end
 
 # capitalize the first letter in each word of a string,
@@ -171,12 +176,14 @@ end
 # 'the lion the witch and the wardrobe' becomes
 # 'The Lion the Witch and the Wardrobe'
 def titleize_a_string(string)
+  string.split
 end
 
 # return true if a string contains any special characters
 # where 'special character' means anything apart from the letters
 # a-z (uppercase and lower) or numbers
 def check_a_string_for_special_characters(string)
+  !!(string =~ /\W/)
 end
 
 # get the upper limit of a range. e.g. for the range 1..20, you
