31f1296e1723bf12579d8200f500a95bc90d255a
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-29 09:07:35 +0100

    three

--- lib/questions.rb
+++ lib/questions.rb
@@ -126,7 +126,9 @@ end
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
-  # Hash[array[0], array[1], array[2], array[3]]
+  hash = {}
+  array.each_slice(2) { |slice| hash[slice[0]]=slice[1] }
+  hash
 end
 
 # get all the letters used in an array of words and return
