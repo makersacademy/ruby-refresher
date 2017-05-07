7839dc9680cfae1615be62478fc412417321b7f5
RSijelmass <R.Sijelmass@gmail.com>
2017-05-07 19:33:24 +0100

    Completes method count_words_of_each_length_in_a_file

--- lib/questions.rb
+++ lib/questions.rb
@@ -311,6 +311,14 @@ end
 # and 1 that is 4 letters long. Return it as a hash in the format
 # word_length => count, e.g. {2 => 1, 3 => 5, 4 => 1}
 def count_words_of_each_length_in_a_file(file_path)
+  lengths = Hash.new { |length, count| length[count] = 0 }
+
+  File.open(file_path, "r") do |file|
+     while line = file.gets
+       line.gsub(/[^A-Za-z0-9]/, " ").split(" ").each{ |word| lengths[word.length] += 1 }
+     end
+   end
+  lengths
 end
 
 # implement fizzbuzz without modulo, i.e. the % method
