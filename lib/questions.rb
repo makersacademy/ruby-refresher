0a840ca2f4bf271228a7494b2e2c9b0133541741
Paula <pmuldoon@umich.edu>
2017-05-26 21:32:59 +0100

    first test passed

--- lib/questions.rb
+++ lib/questions.rb
@@ -8,6 +8,11 @@ end
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
+  array.each do |item|
+    if item == nil
+      array.delete(item)
+    end
+  end
 end
 
 # remove instances of nil AND false from an array
