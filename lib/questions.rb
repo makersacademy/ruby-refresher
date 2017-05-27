dde20f4cea246acc264644c6b4ca255174b8c498
Filipe Fasolin <ffasolin@gmail.com>
2017-05-27 14:13:39 +0100

    19th done

--- lib/questions.rb
+++ lib/questions.rb
@@ -114,6 +114,9 @@ end
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  f = array.reduce(:+).to_f / array.length.to_f
+  i = f.to_i
+  f - i > 0.5 ? i + 1 : i
 end
 
 # get all the elements in an array, up until the first element
