429d59d3b92e9b69d3711eb65b10fcf856f23717
Filipe Fasolin <ffasolin@gmail.com>
2017-05-27 15:34:06 +0100

    29th done

--- lib/questions.rb
+++ lib/questions.rb
@@ -183,6 +183,8 @@ end
 # get the domain name *without* the .com part, from an email address
 # so alex@makersacademy.com becomes makersacademy
 def get_domain_name_from_email_address(email)
+  at_index = email.rindex("@")
+  email[at_index+1...-4]
 end
 
 # capitalize the first letter in each word of a string,
