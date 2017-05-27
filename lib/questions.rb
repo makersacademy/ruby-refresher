2d60245d8cdd42b4d7caa54ff9b3a3b573797065
Paula <pmuldoon@umich.edu>
2017-05-27 15:22:25 +0100

    18 failures

--- lib/questions.rb
+++ lib/questions.rb
@@ -165,15 +165,12 @@ end
 def remove_capital_letters_from_string(string)
 end
 
-# round up a float up and convert it to an Integer,
-# so 3.214 becomes 4
 def round_up_number(float)
   float.ceil
 end
 
-# round down a float up and convert it to an Integer,
-# so 9.52 becomes 9
 def round_down_number(float)
+  float.to_i
 end
 
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
