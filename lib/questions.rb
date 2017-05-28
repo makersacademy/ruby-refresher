4a4228bb91ce66106a7ce955f9599fa58f782874
BenNoonan1991 <ben.noonan2@gmail.com>
2017-05-28 18:48:45 +0100

    7 failures remaining

--- lib/questions.rb
+++ lib/questions.rb
@@ -197,11 +197,13 @@ end
 # where 'special character' means anything apart from the letters
 # a-z (uppercase and lower) or numbers
 def check_a_string_for_special_characters(string)
+  /\W/ === string
 end
 
 # get the upper limit of a range. e.g. for the range 1..20, you
 # should return 20
 def get_upper_limit_of(range)
+  range.end
 end
 
 # should return true for a 3 dot range like 1...20, false for a
@@ -211,6 +213,7 @@ end
 
 # get the square root of a number
 def square_root_of(number)
+  Math.sqrt(number)
 end
 
 # count the number of words in a file
