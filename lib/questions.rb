b7813ca86f03b5fc1d04ae7597d221c069ff3f69
Unai Motriko <unaimotriko@gmail.com>
2017-05-28 14:21:06 +0100

    It solves questions 1 to 22

--- lib/questions.rb
+++ lib/questions.rb
@@ -66,8 +66,7 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
-  array.map { |x| x.even?  }
-
+  array = array.select { |x| x.even?  } , array.select { |x| x.odd?  }
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -75,7 +74,8 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
-
+  array = array.each { |x| array.delete(x) if x != x.reverse }
+  return array.length
 end
 
 # return the shortest word in an array
@@ -116,12 +116,14 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
+ array = array.take_while { |x| x <= 5}
 end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+  array =  Hash[*array.flatten(1)]
 end
 
 # get all the letters used in an array of words and return
@@ -129,12 +131,14 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
+  array.map! { |x| x.split(//)}.flatten.uniq.sort
 end
 
 # swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
+
 end
 
 # in a hash where the keys and values are all numbers
