a451483d1ed3e1844c2e8e8b36672d8cad3bd681
Robert Jones <robert.g.jones@gmail.com>
2017-05-28 11:03:50 +0100

    remove nils / refactor vowels

--- lib/questions.rb
+++ lib/questions.rb
@@ -5,11 +5,12 @@ end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
-  array.select {|str| str.chars.first =~ /\b[aeiou]/ }
+  array.select {|str| str =~ /\b[aeiou]/ }
 end
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
+  array.compact
 end
 
 # remove instances of nil AND false from an array
