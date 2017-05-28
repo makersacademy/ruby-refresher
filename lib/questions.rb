0d779ae07ae9c59327d35057dd03c0e155b00a06
Robert Jones <robert.g.jones@gmail.com>
2017-05-28 11:09:19 +0100

    remove nils and false; reverse words in place in array

--- lib/questions.rb
+++ lib/questions.rb
@@ -15,11 +15,13 @@ end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
+  array.compact.reject{ |i| i == false }
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(array)
+  array.map { |word| word.reverse }
 end
 
 # given an array of student names, like ['Bob', 'Dave', 'Clive']
