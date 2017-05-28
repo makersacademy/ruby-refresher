0af46ec27137ff96ca0e64c1e868ed89ffbff95e
Unai Motriko <unaimotriko@gmail.com>
2017-05-28 11:47:25 +0100

    It solves second question

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,9 +1,10 @@
 # keep only the elements that start with an a
 def select_elements_starting_with_a(array)
-  array.each { | x | array.delete(x) if x.chars.first != 'a'}
+  array.each { | x | array.delete(x) if x[0] != 'a' }
 end
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+    array.select { | x | /[aeoui]/.match(x[0]) }
 end
 
 # remove instances of nil (but NOT false) from an array
