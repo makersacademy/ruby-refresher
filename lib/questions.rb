fa38064e60264c28e42f72714a7cb5f47ccc30bf
Kynosaur <kye.bracey@gmail.com>
2017-06-24 21:10:16 +0100

    Completes convert_array_to_a_hash method

--- lib/questions.rb
+++ lib/questions.rb
@@ -128,6 +128,9 @@ end
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+  keys = array.select.with_index { |element, index| index.even? }
+  values = array.select.with_index { |element, index| index.odd? }
+  keys.zip(values).to_h
 end
 
 # get all the letters used in an array of words and return
