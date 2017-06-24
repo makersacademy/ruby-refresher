efd6eea57df85d41d33bd0435c0550bffe5a2a23
bannastre <bannastre@gmail.com>
2017-06-24 19:18:56 +0100

    All tests passed

---
+++ data/bank_holidays.rb
@@ -0,0 +1,9 @@
+@bank_holidays = [ ["26/12/2014", 'Boxing Day'],
+                  ["25/12/2014", "Christmas Day"],
+                  ["25/08/2014", "Summer bank holiday"],
+                  ["26/05/2014", "Spring bank holiday"],
+                  ["05/05/2014", "Early May bank holiday"],
+                  ["21/04/2014", "Easter Monday"],
+                  ["18/04/2014", "Good Friday"],
+                  ["01/01/2014", "New Year’s Day"]
+                ]
