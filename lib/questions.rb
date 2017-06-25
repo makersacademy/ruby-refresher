1b4d382aaf497905dc2448ea7819460e99d89f57
Kavita <kkavita92@gmail.com>
2017-06-25 12:45:41 +0100

    Complete new problem: Birthday is on Friday

--- lib/questions.rb
+++ lib/questions.rb
@@ -258,6 +258,10 @@ end
 # e.g. january 1st, will next be a friday in 2016
 # return the day as a capitalized string like 'Friday'
 def your_birthday_is_on_a_friday_in_the_year(birthday)
+  until birthday.friday?
+    birthday = Time.new(birthday.year + 1, birthday.month, birthday.day)
+  end
+  birthday.year
 end
 
 # in a file, total the number of times words of different lengths
