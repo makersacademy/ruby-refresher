f683cc3e36eb31f3f9dfa33ffccc8af70ca900e9
Mutsa <mutsa.evelyn@hotmail.com>
2017-08-21 08:29:29 +0100

    Completed an additional 6 questions

--- lib/questions.rb
+++ lib/questions.rb
@@ -120,6 +120,8 @@ end
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+  Hash[*array] # * converts he array into an argument list to turn into a hash
+  # array.each_slice(2).to_h also works
 end
 
 # get all the letters used in an array of words and return
@@ -133,27 +135,32 @@ end
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
+  hash.invert
 end
 
 # in a hash where the keys and values are all numbers
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
+  hash.map {|key, value| key + value }.compact.reduce(:+)
 end
 
 # take out all the capital letters from a string
 # so 'Hello JohnDoe' becomes 'ello ohnoe'
 def remove_capital_letters_from_string(string)
+  string.tr("A-Z", "")
 end
 
 # round up a float up and convert it to an Integer,
 # so 3.214 becomes 4
 def round_up_number(float)
+  float.ceil
 end
 
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+  float.floor
 end
 
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
