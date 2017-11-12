dbfbc2234c2ab8dbf6e9ab6cae46c270938cb50f
Canace Wong <canacewongtoday@hotmail.com>
2017-11-12 17:13:00 +0000

    some questions passed

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,22 +1,30 @@
 # keep only the elements that start with an a
 def select_elements_starting_with_a(array)
+  array.select { |item| item[0] == 'a'}
 end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+  array.select do |item|
+    item[0] == 'a' || item [0] =='e' || item[0] == 'i' || item [0] == 'o' || item [0] =='u'
+  end
 end
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
+  array.select {|item| item != nil}
 end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
+  array.select {|item| item != nil && item != false}
+
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(array)
+  array.map { |item| item.reverse! }
 end
 
 # given an array of student names, like ['Bob', 'Dave', 'Clive']
@@ -29,21 +37,31 @@ end
 # discard the first 3 elements of an array,
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
+  array[3..-1]
 end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+  array.insert(0, element)
 end
 
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
+  array.sort_by {|item| item.split.last }
 end
 
 # cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  str_array = string.split("")
+  if str_array.length.even?
+    str_array_half = str_array[0..((str_array.length/2)-1)]
+  else
+    str_array_half = str_array[0..((str_array.length)/2)]
+  end
+  str_array_half.join("")
 end
 
 # turn a positive integer into a negative integer. A negative integer
