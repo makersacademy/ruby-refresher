701efc8319bc397ce2c9128a9861053f2e1f6ffc
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-03-10 18:00:54 +0000

    Solve #4 and #5

--- lib/questions.rb
+++ lib/questions.rb
@@ -15,11 +15,14 @@ end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
+  array.delete_if { |elem| elem == false }
+  array.compact
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(array)
+  array.each { |str| str.reverse! }
 end
 
 # given an array of student names, like ['Bob', 'Dave', 'Clive']
